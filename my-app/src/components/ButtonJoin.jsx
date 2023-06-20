0f27e66f0e9216e3c1b6694fbab3590f2a9ad75f
Yana Tolstobrova <jana.tolstobrova@gmail.com>
2023-06-20 12:51:26 +0200

    responsive tablet

--- my-app/src/components/ButtonJoin.jsx
+++ my-app/src/components/ButtonJoin.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 export function ButtonJoin() {
   return (
-      <div>
+      <div className='flex md:justify-center xl:justify-start'> 
           <Link to={'/join'}><button className='bg-button-bg py-3 px-6 rounded-xl hover:bg-button-hover focus:bg-button-focus'>Join Now</button></Link>
       
     </div>
--- my-app/src/components/MainJoin.jsx
+++ my-app/src/components/MainJoin.jsx
@@ -5,7 +5,7 @@ export function MainJoin() {
   return (
     <div className='flex flex-col w-6/6 mt-4 mx-4 px-4 xl:w-4/6 xl:mx-auto xl:my-[9em] xl:flex-row xl:justify-between xl:px-0 xl:items-center'>
     
-        <h1 className='text-6xl self-start font-bold text-white w-6/6 z-20 xl:w-3/6 xl:self-center' >Join the <span className='text-accent-text'>fun.</span></h1>
+        <h1 className='text-6xl self-start md:self-center font-bold text-white w-6/6 z-20 xl:w-3/6 xl:self-center' >Join the <span className='text-accent-text'>fun.</span></h1>
       <SignupForm />
         
     </div>
