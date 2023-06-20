29cffb9438ac4cd8787ddb9d89e6bbb128aff768
Yana Tolstobrova <jana.tolstobrova@gmail.com>
2023-06-20 11:37:33 +0200

    form validate input without text

--- my-app/src/components/Form.jsx
+++ my-app/src/components/Form.jsx
@@ -25,17 +25,17 @@ import { useFormik } from 'formik';
 const validate = values => {
   const errors = {};
 
-  if (!values.name) {
+  if (!values.name || !values.name.trim()) {
     errors.name = 'Required';
   }
 
-  if (!values.email) {
+  if (!values.email || !values.email.trim()) {
       errors.email = 'Required';
     } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
       errors.email = 'Invalid email address';
   }
   
-  if (!values.password) {
+  if (!values.password || !values.password.trim()) {
     errors.password = 'Required';
   } else if (!/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/i.test(values.password)) {
     errors.password = 'Must be minimum 8 characters, have at least 1 letter and 1 number';
