c46e78a5ffda40fd9e70b7734a98c468b561ac35
Yana Tolstobrova <jana.tolstobrova@gmail.com>
2023-06-22 10:45:08 +0200

    some changes to styles

--- my-app/src/components/Footer.jsx
+++ my-app/src/components/Footer.jsx
@@ -4,14 +4,14 @@ import Facebook from '../assets/images/facebook.svg';
 export function Footer() {
   return (
     <div className='text-white font-semibold bg-footer-form-icon-bg z-20 relative'>
-      <div className='flex items-center justify-around py-4 w-6/6 mx-4 xl:w-4/6 xl:mx-auto xl:py-7 xl:justify-between'>
+      <div className='flex items-center justify-around py-4 w-6/6 mx-4 md:w-4/6 md:mx-auto md:py-7 md:justify-between'>
             <ul className='flex'>
             <li className='mr-5'>About us</li>
             <li>Contact</li>
           </ul>
           <ul className='flex'>
             <li className='flex flex-col items-center mr-5 xl:flex-row'><img src={Twitter} alt='twitter icon' className='h-[1.2em] w-[1.2em] mb-[0.1em] xl:mr-3 xl:mb-0'/>Twitter</li>
-            <li className='flex flex-col items-center mr-5 xl:flex-row'><img src={Facebook} alt='facebook icon' className=' h-[1.2em] w-[1.2em] mb-[0.15em] xl:mr-3 xl:mb-0'/>Facebook</li>
+            <li className='flex flex-col items-center xl:flex-row'><img src={Facebook} alt='facebook icon' className=' h-[1.2em] w-[1.2em] mb-[0.15em] xl:mr-3 xl:mb-0'/>Facebook</li>
           </ul>
       </div>
       
--- my-app/src/components/MainHome.jsx
+++ my-app/src/components/MainHome.jsx
@@ -7,7 +7,7 @@ export function MainHome() {
       <div className='w-6/6 z-20 relative mt-[7em] mb-[15em] flex flex-col justify-start md:mt-[6em] mb-[14em] xl:flex-row xl:mb-0 xl:w-4/6 xl:mx-auto xl:justify-between'>
         <img src={Girl} alt='girl with voluminous hair in profile' className='hidden z-10 xl:block xl:w-[25rem] '></img>
   
-        <div className='text-white pt-20 px-4 xl:mx-0'>
+        <div className='text-white pt-20 px-4 xl:mx-0 xl:w-3/6'>
               <h1 className='text-5xl md:text-center xl:text-left'>Feel The Music</h1>
               <p className='my-5 text-xl w-[28rem] md:mx-auto md:text-center xl:text-left'>Stream over 20 thousand songs with one click</p>
               <ButtonJoin/>
